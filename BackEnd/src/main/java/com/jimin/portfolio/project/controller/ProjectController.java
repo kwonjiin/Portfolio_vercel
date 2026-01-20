65976cb04f1b96747cb0f40a9c6a5223aa284218
DESKTOP-KKJ0FNN\jimin <jimin001006@naver.com>
2026-01-20 17:40:17 +0900

    배포테스트4

--- BackEnd/src/main/java/com/jimin/portfolio/project/controller/ProjectController.java
+++ BackEnd/src/main/java/com/jimin/portfolio/project/controller/ProjectController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/projects")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://api.kwonjiminportfolio.com")
 public class ProjectController {
 
     private final ProjectService projectService;
